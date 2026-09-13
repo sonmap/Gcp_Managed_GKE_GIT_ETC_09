d65fe3d7e9ca4d4e2adafaa01ee224b15d87f12c
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-13 20:52:46 +0900

    refactor: manage existing project IAM in admin layer

--- terraform/00-admin/iam/main.tf
+++ terraform/00-admin/iam/main.tf
@@ -1,5 +1,11 @@
 locals {
   cloud_run_service_agent = "service-${var.cicd_project_number}@serverless-robot-prod.iam.gserviceaccount.com"
+
+  project_factory_roles = toset([
+    "roles/browser",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/serviceusage.serviceUsageAdmin",
+  ])
 }
 
 resource "google_compute_subnetwork_iam_member" "cloud_run_network_user" {
@@ -14,4 +20,14 @@ resource "google_project_iam_member" "cloud_run_network_viewer" {
   project = var.shared_vpc_host_project_id
   role    = "roles/compute.networkViewer"
   member  = "serviceAccount:${local.cloud_run_service_agent}"
+}
+
+# This root is executed by a top-level administrator. The lower-privileged
+# 01-foundation executor cannot bootstrap IAM on an existing business project.
+resource "google_project_iam_member" "project_factory_existing_project_roles" {
+  for_each = local.project_factory_roles
+
+  project = var.existing_sandbox_project_id
+  role    = each.value
+  member  = "serviceAccount:${var.project_factory_service_account}"
 }
