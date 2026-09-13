2a5e5d85847e1990c7fd14fd386f8dc125f572cd
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-13 16:14:48 +0900

    fix: allow Cloud Build to publish platform images

--- terraform/01-foundation/artifact_registry.tf
+++ terraform/01-foundation/artifact_registry.tf
@@ -6,3 +6,10 @@ resource "google_artifact_registry_repository" "platform" {
   depends_on    = [google_project_service.cicd]
 }
 
+resource "google_artifact_registry_repository_iam_member" "cloud_build_writer" {
+  project    = var.cicd_project_id
+  location   = var.region
+  repository = google_artifact_registry_repository.platform.name
+  role       = "roles/artifactregistry.writer"
+  member     = "serviceAccount:${data.google_project.cicd.number}-compute@developer.gserviceaccount.com"
+}
