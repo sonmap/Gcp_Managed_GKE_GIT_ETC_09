593b7095f5c1bf315e719f1539592aebf8b9a9d8
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-13 21:15:47 +0900

    docs: expose network administrator IAM outputs

--- terraform/00-admin/iam/outputs.tf
+++ terraform/00-admin/iam/outputs.tf
@@ -5,3 +5,14 @@ output "existing_sandbox_project_id" {
 output "project_factory_roles" {
   value = sort(tolist(local.project_factory_roles))
 }
+
+output "shared_vpc_admin_folder_id" {
+  value = var.shared_vpc_admin_folder_id
+}
+
+output "network_admin_roles" {
+  value = concat(
+    sort(tolist(local.network_admin_host_roles)),
+    ["roles/compute.xpnAdmin"],
+  )
+}
