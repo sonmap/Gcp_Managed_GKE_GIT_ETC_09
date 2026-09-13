6ea2d79aaf2aa96223e1371b0872a239187eb134
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-13 20:40:28 +0900

    fix: allow existing project without folder billing inputs

--- terraform/02-sandbox/deployments/10-project/variables.tf
+++ terraform/02-sandbox/deployments/10-project/variables.tf
@@ -1,8 +1,14 @@
 variable "create_project" { type = bool }
 variable "project_id" { type = string }
 variable "project_name" { type = string }
-variable "folder_id" { type = string }
-variable "billing_account" { type = string }
+variable "folder_id" {
+  type    = string
+  default = null
+}
+variable "billing_account" {
+  type    = string
+  default = null
+}
 variable "task_name" { type = string }
 variable "expires_on" { type = string }
 variable "region" { type = string }
