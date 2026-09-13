7284263c79715d183f6618e3b51a777825cbfd6f
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-13 20:52:45 +0900

    refactor: manage existing project IAM in admin layer

--- terraform/00-admin/iam/variables.tf
+++ terraform/00-admin/iam/variables.tf
@@ -2,16 +2,31 @@ variable "shared_vpc_host_project_id" {
   type    = string
   default = "pjt-d-shared-base"
 }
+
 variable "region" {
   type    = string
   default = "asia-northeast3"
 }
+
 variable "cloudrun_subnet_name" {
   type    = string
   default = "subnet-cloudrun-egress-an3"
 }
+
 variable "cicd_project_number" {
   description = "Numeric project number of prj-b-cicd-local-236d."
   type        = string
   default     = "587273205772"
+}
+
+variable "existing_sandbox_project_id" {
+  description = "Existing project approved for sandbox data resources."
+  type        = string
+  default     = "pjt-net-hub-base"
+}
+
+variable "project_factory_service_account" {
+  description = "Infrastructure Manager identity that prepares an existing sandbox project."
+  type        = string
+  default     = "sa-im-project-factory@prj-b-cicd-local-236d.iam.gserviceaccount.com"
 }
