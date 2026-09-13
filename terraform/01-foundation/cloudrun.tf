62412adfad9cb78c33210786540c69ed1697fbe5
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-13 16:43:11 +0900

    fix: disable Cloud Run deletion protection for failed create recovery

--- terraform/01-foundation/cloudrun.tf
+++ terraform/01-foundation/cloudrun.tf
@@ -20,7 +20,8 @@ resource "google_cloud_run_v2_service" "provisioner" {
   project  = var.cicd_project_id
   name     = "run-sandbox-provisioner"
   location = var.region
-  ingress  = var.provisioner_ingress
+  ingress             = var.provisioner_ingress
+  deletion_protection = false
 
   template {
     service_account = google_service_account.automation["api"].email
