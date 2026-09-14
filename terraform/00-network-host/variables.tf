4f3946031b864583243e3b402fc9d2f3e7dd6bc9
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-14 13:42:41 +0900

    feat(network): add 10.x platform subnets and internal ALB ranges

--- terraform/00-network-host/variables.tf
+++ terraform/00-network-host/variables.tf
@@ -1,10 +1,28 @@
 variable "shared_vpc_host_project_id" { type = string }
 variable "network_name" { type = string }
 variable "region" { type = string }
-variable "gke_subnet_name" { type = string }
+
+variable "internal_alb_frontend_subnet_name" { type = string }
+variable "internal_alb_frontend_subnet_cidr" { type = string }
+variable "jupyter_internal_alb_ip_name" { type = string }
+variable "jupyter_internal_alb_ip" { type = string }
+variable "internal_alb_proxy_subnet_name" { type = string }
+variable "internal_alb_proxy_subnet_cidr" { type = string }
+
+variable "gke_main_subnet_name" { type = string }
+variable "gke_main_subnet_cidr" { type = string }
+variable "gke_main_pod_range_name" { type = string }
+variable "gke_main_pod_cidr" { type = string }
+variable "gke_test_subnet_name" { type = string }
+variable "gke_test_subnet_cidr" { type = string }
+variable "gke_test_pod_range_name" { type = string }
+variable "gke_test_pod_cidr" { type = string }
+
+variable "cloudrun_subnet_name" { type = string }
 variable "cloudrun_subnet_cidr" { type = string }
+variable "cloudbuild_psa_name" { type = string }
 variable "cloudbuild_psa_cidr" { type = string }
+
 variable "task_name" { type = string }
 variable "task_subnet_cidr" { type = string }
 variable "task_vm_service_account_email" { type = string }
-variable "gke_pod_cidr" { type = string }
