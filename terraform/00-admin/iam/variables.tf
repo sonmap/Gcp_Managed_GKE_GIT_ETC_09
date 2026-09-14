2e5f1356b034acc6c622b206dc44012f433b2da1
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-14 14:04:36 +0900

    feat(iam): add workflow cross-project variables

--- terraform/00-admin/iam/variables.tf
+++ terraform/00-admin/iam/variables.tf
@@ -16,7 +16,7 @@ variable "region" {
 
 variable "cloudrun_subnet_name" {
   type    = string
-  default = "subnet-cloudrun-egress-an3"
+  default = "subnet-dev-cicd-run-01-an3-egress"
 }
 
 variable "cicd_project_number" {
@@ -25,10 +25,22 @@ variable "cicd_project_number" {
   default     = "587273205772"
 }
 
+variable "gke_project_id" {
+  description = "Project containing the main JupyterHub GKE Autopilot cluster."
+  type        = string
+  default     = "pjt-d-host01"
+}
+
 variable "existing_sandbox_project_id" {
   description = "Existing project approved for sandbox data resources."
   type        = string
   default     = "pjt-net-hub-base"
+}
+
+variable "workflow_service_account" {
+  description = "Workflow runtime identity created in prj-b-cicd-local-236d."
+  type        = string
+  default     = "sa-sandbox-workflow@prj-b-cicd-local-236d.iam.gserviceaccount.com"
 }
 
 variable "project_factory_service_account" {
