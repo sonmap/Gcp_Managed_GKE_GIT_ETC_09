6469f88aa191f68a25e810fb0d0c92f487cb8e25
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-14 23:42:43 +0900

    Grant network automation Shared VPC admin

--- terraform/00-admin/iam/main.tf
+++ terraform/00-admin/iam/main.tf
@@ -18,6 +18,7 @@ locals {
   network_admin_host_roles = toset([
     "roles/compute.networkAdmin",
     "roles/compute.securityAdmin",
+    "roles/compute.xpnAdmin",
   ])
 
   workflow_gke_project_roles = toset([
