1190bee6f3ee3b229a50a826f8da7058eb95e535
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-16 19:22:53 +0900

    fix: parameterize regional internal Jupyter ALB

--- terraform/02-sandbox/deployments/60-loadbalancer/variables.tf
+++ terraform/02-sandbox/deployments/60-loadbalancer/variables.tf
@@ -2,3 +2,23 @@ variable "gke_project_id" { type = string }
 variable "task_name" { type = string }
 variable "jupyter_domain" { type = string }
 variable "neg_self_links" { type = list(string) }
+
+variable "region" {
+  type    = string
+  default = "asia-northeast3"
+}
+
+variable "shared_vpc_host_project_id" {
+  type    = string
+  default = "pjt-d-shared-base"
+}
+
+variable "shared_vpc_network_name" {
+  type    = string
+  default = "vpc-d-shared-base"
+}
+
+variable "frontend_subnet_name" {
+  type    = string
+  default = "subnet-dev-sbx-ing-01-an3-ilb"
+}
